4827aef4f3f5462325440c94a10201cfe7cd1616
Sharita <sm_bernier@icloud.com>
2022-10-18 20:27:38 -0400

    matching pairs

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 //<-- User Interactive Game Introduction -->>//
 
-// let playerName = prompt("Enter your name, and we'll start matching world continents");
-// let displayTitle = document.querySelector(`.mgTitle`)
-//     displayTitle.innerText = `${playerName}'s Continent Match Challenge`
-// let playerMatch = document.querySelector(`.playerMatch`)
-//     playerMatch.innerText = `${playerName}'s Match total`
+let playerName = prompt("Enter your name, and we'll start matching world continents");
+let displayTitle = document.querySelector(`.mgTitle`)
+    displayTitle.innerText = `${playerName}'s Continent Match Challenge`
+let playerMatch = document.querySelector(`.playerMatch`)
+    playerMatch.innerText = `${playerName}'s Match total`
 
 //<-- Global Variables -->>//
 const gameCards = document.querySelectorAll(`.gameCard`);
@@ -12,14 +12,14 @@ let card_Front = document.querySelector(`.card_Front`);
 let card_Back = document.querySelector(`.card_Back`);
 
 //-----WINNING PAIRS----//
-let africaPair = document.getElementById(`#c0, #c14`);
-let nAmericaPair = document.getElementById(`#c5 , #c11`);
-let australiaPair = document.getElementById(`#c7 , #c9`);
-let sAmericaPair = document.getElementById(`#c3 , #c15`);
-let asiaPair = document.getElementById(`#c13 , #c8`);
-let europePair = document.getElementById(`#c1 , #c2`);
-let antarcticaPair = document.getElementById(`#c6 , #c10`)
-let compassPair = document.getElementById(`#c4 , #c12`);
+// let africaPair = document.getElementById(`#c0, #c14`);
+// let nAmericaPair = document.getElementById(`#c5 , #c11`);
+// let australiaPair = document.getElementById(`#c7 , #c9`);
+// let sAmericaPair = document.getElementById(`#c3 , #c15`);
+// let asiaPair = document.getElementById(`#c13 , #c8`);
+// let europePair = document.getElementById(`#c1 , #c2`);
+// let antarcticaPair = document.getElementById(`#c6 , #c10`)
+// let compassPair = document.getElementById(`#c4 , #c12`);
 //<-- Game Functions -->>//
 
 let hasFlippedCard = false;
@@ -31,7 +31,37 @@ function flipCard () {
     if (!hasFlippedCard){
         hasFlippedCard = true;
         firstCard = this;
+        return;
     }
+
+    secondCard = this;
+    hasFlippedCard = false;
+
+    checkForMatch();
+}
+
+function checkForMatch (){
+    if(firstCard.dataset.framework === secondCard.dataset.framework){
+        //I would like to the card to flip, and then alert
+        alert(`${playerName}, you have a match!`)
+        disableCards();
+        return;
+    } else {
+        alert(`Nope! Sorry ${playerName}, try again.`)
+    }
+    unFlipCards();
+}
+
+function disableCards () {
+    firstCard.removeEventListener(`click`, flipCard);
+    secondCard.removeEventListener(`click`, flipCard);
+}
+
+function unFlipCards () {
+    setTimeout (() => {
+        firstCard.classList.remove(`flip`);
+        secondCard.classList.remove(`flip`);
+    }, 1000);
 }
 
 gameCards.forEach(card => card.addEventListener(`click`, flipCard))
