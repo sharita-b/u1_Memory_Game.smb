0d4c5a096c3e01cd180ac7dbd9b986b1dfcbc949
Sharita <sm_bernier@icloud.com>
2022-10-19 11:25:30 -0400

    gameReset function framwork

--- script.js
+++ script.js
@@ -11,22 +11,28 @@ const gameCards = document.querySelectorAll(`.gameCard`);
 let card_Front = document.querySelector(`.card_Front`);
 let card_Back = document.querySelector(`.card_Back`);
 const displayTime = document.querySelector(`#displayTime`);
+const displayMatch = document.querySelector(`#displayMatch`)
 
-
+//Simple Game Timer to Start// -- would like to add response
 let time = 100;
 setInterval(gameTimeLeft, 1000);
 function gameTimeLeft () {
-displayTime.innerHTML = `${time}`; time--;
+    displayTime.innerHTML = `${time}`; 
+    time--;
+    if (time === 0){
+       //call the reset function// 
+    }
+
 }
 gameTimeLeft()
 
 
+//Empty first & second Card variables allowed the flipCard funtion below. **Learning** use of truthy manipulation in the if/else statement is what made this possible** -> Help from TA T.P.
 let hasFlippedCard = false;
 let firstCard, secondCard;
 
 function flipCard () {
     this.classList.add(`flip`);
-//Empty first & second Card variables allowed the flipCard funtion below. **Learning** use of truthy manipulation in the if/else statement is what made this possible** -> Help from TA T.P.
     if (!hasFlippedCard){
         hasFlippedCard = true;
         firstCard = this;
@@ -65,7 +71,17 @@ function unFlipCards () {
 
 gameCards.forEach(card => card.addEventListener(`click`, flipCard))
 
-
+//This scope should be bigger, to include all game functions. I think
+function gameReset () {
+    if (time === 0){
+      gameCards.classList.remove(`flip`);
+      //reset match total//
+    } else if (displayMatch === 8){
+      gameCards.classList.remove(`flip`);
+      alert(`${playerName}, you WIN!`);
+      //additional graphic celebration would be nice
+    }
+  }
 
 
 
